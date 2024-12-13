1dd766d55a0dfed3d7c15b8eaa6e844d4c57fb4a
anhlh74 <anhlhfw@gmail.com>
2024-12-13 16:01:46 +0700

    test deploy

--- backend/db/index.js
+++ backend/db/index.js
@@ -12,7 +12,7 @@ exports.connect = (app) => {
 
   const connectWithRetry = () => {
     mongoose.Promise = global.Promise;
-    console.log("MongoDB connection with retryyyyyy");
+    console.log("MongoDB connection with retryyyyyyaaaa");
     mongoose
       .connect(process.env.MONGODB_URI, options)
       .then(() => {
